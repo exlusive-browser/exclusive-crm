94de0271b119667d2524324a0ebe239347823a54
Alejandro Salas <ainfantem@uninorte.edu.co>
2024-11-03 09:12:23 -0500

    Add Client List

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -1,11 +1,36 @@
 import Button, { ButtonProps } from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
+import { Link } from "react-router-dom";
 
 export const PrimaryButton = styled((props: ButtonProps) => (
   <Button color="primary" variant="contained" {...props}>
     {props?.children}
   </Button>
 ))(({ theme }) => ({
+  fontWeight: "normal",
+  borderRadius: theme.spacing(1.5),
+  padding: theme.spacing(1),
+  fontSize: theme.typography.pxToRem(13),
+}));
+
+type ButtonPropsWithLink = ButtonProps & {
+  navigateTo: string;
+};
+
+export const PrimaryLinkButton = styled((props: ButtonPropsWithLink) => {
+  const { navigateTo, ...rest } = props;
+  return (
+    <Button
+      color="primary"
+      variant="contained"
+      to={navigateTo}
+      component={Link}
+      {...rest}
+    >
+      {props?.children}
+    </Button>
+  );
+})(({ theme }) => ({
   fontWeight: "normal",
   borderRadius: theme.spacing(1.5),
   padding: theme.spacing(1),
--- src/features/client/pages/ClientList/ClientListPage.tsx
+++ src/features/client/pages/ClientList/ClientListPage.tsx
@@ -1,7 +1,39 @@
+import AddIcon from "@mui/icons-material/Add";
+import { Container, Stack, Typography } from "@mui/material";
+import { FullPageBox } from "../../../../components/Layout/FullPageBox";
+import { PrimaryLinkButton } from "../../../../components/buttons";
+import { ClientsTable } from "./ClientTable";
+
 export function ClientListPage() {
   return (
-    <div>
-      <p>Clien tList Page</p>
-    </div>
+    <FullPageBox>
+      <Container
+        maxWidth="xl"
+        sx={{ display: "flex", flexDirection: "column" }}
+      >
+        <Stack direction="row" justifyContent="space-between" my={4}>
+          <Typography
+            variant="h1"
+            component="h1"
+            sx={{
+              fontFamily: "titleFontFamily",
+              fontWeight: "medium",
+              fontSize: { xs: 28, lg: 36 },
+            }}
+          >
+            Clients
+          </Typography>
+          <PrimaryLinkButton
+            size="small"
+            sx={{ px: 2 }}
+            startIcon={<AddIcon />}
+            navigateTo="/clients/create"
+          >
+            Create new
+          </PrimaryLinkButton>
+        </Stack>
+        <ClientsTable />
+      </Container>
+    </FullPageBox>
   );
 }
