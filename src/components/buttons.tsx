5bc79b3e778073f777ce115af01f20cd52aa712f
dgop92 <inevaup@gmail.com>
2024-11-03 22:27:20 -0500

    hotfix: fix duplicate types in buttons.tsx

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -46,27 +46,3 @@ export const SecondaryButton = styled((props: ButtonProps) => (
   borderRadius: theme.spacing(1.75),
   p: 1.5,
 }));
-
-type ButtonPropsWithLink = ButtonProps & {
-  navigateTo: string;
-};
-
-export const PrimaryLinkButton = styled((props: ButtonPropsWithLink) => {
-  const { navigateTo, ...rest } = props;
-  return (
-    <Button
-      color="primary"
-      variant="contained"
-      to={navigateTo}
-      component={Link}
-      {...rest}
-    >
-      {props?.children}
-    </Button>
-  );
-})(({ theme }) => ({
-  fontWeight: "normal",
-  borderRadius: theme.spacing(1.5),
-  padding: theme.spacing(1),
-  fontSize: theme.typography.pxToRem(13),
-}));
